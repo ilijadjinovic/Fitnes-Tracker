14421e9257dcf32ac269972acc283566698188f6
ilijadjinovic <ilija.djinovic@gmail.com>
2026-08-22 12:43:24 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,5 @@
+// ===== DAYS — dani u nedelji =====
+const DAYS = ['Pon','Uto','Sre','Čet','Pet','Sub','Ned'];
+
+// ===== LOGO — putanja do ikonice aplikacije =====
+const LOGO = 'icons/icon-192.png';
